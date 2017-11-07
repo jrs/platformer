4dda852e2bfc4027b00c2a881433250982b78fce
Justin Scott <jrs@users.noreply.github.com>
2017-11-07 07:36:57 -0800

    upload

---
+++ offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1510067654,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/alien-sheet0.png",
+		"images/coin-sheet0.png",
+		"images/groundtop-sheet0.png",
+		"images/ground-sheet0.png",
+		"images/grass-sheet0.png",
+		"images/sign-sheet0.png",
+		"images/cloud-sheet0.png",
+		"images/qblock-sheet0.png",
+		"images/qblock-sheet1.png",
+		"images/brick-sheet0.png",
+		"images/flag-sheet0.png",
+		"images/flag-sheet1.png",
+		"images/barnacle-sheet0.png",
+		"images/barnacle-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
